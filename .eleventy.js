38642eb686dfd97542756fcd8d453c4ad2d97f9a
kelsS <ratherkelsey@gmail.com>
2024-11-08 20:39:42 -0500

    added blog post setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,32 @@
-module.exports = {
-    dir: {
-        input: "src",
-        output: "dist",
-        layouts: 'includes/layouts',
-        includes: "includes",
-		data: "data",
+const { DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+    // Put robots.txt in root
+    eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
+
+    eleventyConfig.addPassthroughCopy({ 'src/styles.css': '/css/styles.css' });
+
+    eleventyConfig.addPassthroughCopy({ 'src/app.js': '/js/app.js' });
+
+    eleventyConfig.addPassthroughCopy({ 'src/assets': '/assets' });
+
+    // Convert gmt date to locale string
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    });
+
+    // Display year
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+	// Input directory: src
+	// Output directory: dist
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+            layouts: 'includes/layouts',
+            includes: "includes",
+            data: "data",
+        }
     }
 };
---
+++ src/blog/2024-11-08-first-blog-post.md
@@ -0,0 +1,15 @@
+---
+layout: 'blog-post.njk'
+title: First blog post
+author: Kelsey S.
+date: 2024-11-08
+image: /assets/blog/snarling-kitty.webp
+imageAlt: Example alt text
+figCaption: Example fig caption
+tags: ["post", "featured"]
+description: here is an example blog post description
+---
+
+# {{ title }}
+
+Example text, there isn't a rule. You just practice and find out which way works best for you. Even the worst thing we can do here is good. We'll make some happy little bushes here. We'll put a happy little sky in here. It's almost like something out of a fairytale book. Let's have a nice tree right here.
