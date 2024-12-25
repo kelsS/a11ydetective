95fec77c214651161054de621a62d92b44742ab7
kelsS <kelsS@users.noreply.github.com>
2024-12-25 00:11:10 +0000

    deploy: a6a41b5601adb4b43726ab2df4e0709f2e6af4f3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
 /**
  * App JS
  */
-
 console.log('Hello from App JS');
+
+// @todo: move to better location within project
+// remove jello class after a few seconds of hovering
+// @todo: figure out performance
+function removeJello() {
+
+const elements = document.querySelectorAll(".interactive");
+
+elements.forEach((item) => {
+        item.addEventListener("mouseover", (event) => {
+
+        setTimeout(() => {
+
+            event.target.classList.remove("jello");
+
+            console.log("Delayed for 1 second.");
+
+            return;
+
+        }, "1000");
+
+            return;
+        });
+
+        return;
+    });
+
+    return;
+}
+  
+window.addEventListener("DOMContentLoaded", () => {
+    removeJello();
+});
